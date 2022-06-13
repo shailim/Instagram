97e38d3b9d916e34094784c34a3f59caa52e64bc
Shaili Manda <shailim@fb.com>
2022-06-13 14:42:14 -0700

    Initial commit

---
+++ app/src/main/java/com/example/instagram/SplashScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.instagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_splash_screen);  // no need because showing splash screen from manifest
+        Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
