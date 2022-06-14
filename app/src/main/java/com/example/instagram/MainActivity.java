8b72eb7dfe385d34b1254b3f4864a7482d56a95d
Shaili Manda <shailim@fb.com>
2022-06-14 14:26:09 -0700

    Parse query to save posts (image not required for now)

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
     private Button btnLogout;
+    private Button btnSubmitPicture;
+    private EditText etDescription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogout = findViewById(R.id.btnLogout);
+        btnSubmitPicture = findViewById(R.id.btnSubmitPicture);
+        etDescription = findViewById(R.id.etDescription);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 ParseUser.logOut();
                 Log.i("Logout", String.valueOf(ParseUser.getCurrentUser()));
+            }
+        });
+
+        btnSubmitPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()) {
+                    Log.i(TAG, "Description cannot be empty");
+                } else {
+                    ParseUser user = ParseUser.getCurrentUser();
+                    savePost(description, user);
+                }
             }
         });
 
@@ -50,6 +69,23 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } else {
                     Log.e(TAG, "Couldn't query posts");
+                }
+            }
+        });
+    }
+
+    public void savePost(String description, ParseUser user) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setUser(user);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    etDescription.setText("");
+                    Log.i(TAG, "post saved!");
+                } else {
+                    Log.e(TAG, "Unable to save post");
                 }
             }
         });
